b6efd04e50a26bbf0b198a86ce3d41f60f4075c5
EC2 Default User <ec2-user@ip-172-31-33-203.ec2.internal>
2020-05-03 23:55:35 +0000

    Initial implementation of get_verified_tweets

--- ViewsUtils.py
+++ ViewsUtils.py
@@ -46,3 +46,35 @@ class ViewsUtils:
             utility_status[ERROR_KEY] = error_msg
             return utility_status
 
+    def get_verified_tweets(self):
+        utility_status = {}
+        try:
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query = [{"user.verified": True}, {"text":1, "user.name":1, "user.followers_count":1}]
+            query_type = "select"
+            result = mongo_con.execute_query(query, query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            utility_status[RESULT_KEY] = []
+
+            for record in result[RESULT_KEY]:
+                if "_id" in record:
+                    del record["_id"]
+                utility_status[RESULT_KEY].append(record)
+
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while fetching verified tweets in viewsutils " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while fetching verified tweets in viewsutils" + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
--- views.py
+++ views.py
@@ -44,5 +44,36 @@ def fetch_tweets_with_hashtags():
 
 #fetch_tweets_with_hashtags()
 
+@application.route('/verified_tweets', methods=['GET', 'POST'])
+def get_verified_tweets():
+    return_status = {}
+    result = []
+    try:
+        return_status = utils_obj.get_verified_tweets()
+
+        if return_status[STATUS_KEY] == STATUS_FAILED:
+            raise Exception(return_status[ERROR_KEY])
+
+        print("Verified tweets:")
+
+        print(return_status[RESULT_KEY])
+
+
+        # return_status[RESULT_KEY] = result
+
+        # print(return_status)
+        return jsonify(return_status)
+
+    except Exception as exp:
+        if ERROR_KEY in return_status:
+            error_msg = "Error while fetching verified tweets in views", str(return_status[ERROR_KEY])
+            print(error_msg)
+        else:
+            error_msg = "Error while fetching verified tweets in views" + str(exp)
+            print(error_msg)
+            return_status[STATUS_KEY] = STATUS_FAILED
+            return_status[ERROR_KEY] = error_msg
+            return jsonify(return_status)
+
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8000)
+    application.run(host='0.0.0.0', port=8001)
