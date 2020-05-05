423a08e05d47dc89d125a90205f70de6e5f09658
Micah <mdemong1@gmail.com>
2020-05-04 18:36:16 -0700

    basic request argument handling for tweets by language

--- views.py
+++ views.py
@@ -11,6 +11,11 @@ ERROR_KEY = "error"
 RESULT_KEY = "result"
 utils_obj = ViewsUtils()
 
+# Default route. For testing if the client can connect to the application.
+@application.route('/')
+def default_route():
+    return "<h1>The Flask application is running successfully! 👍</h1>"
+
 @application.route('/tweets_with_hashtags', methods=['GET', 'POST'])
 def fetch_tweets_with_hashtags():
     return_status = {}
@@ -66,10 +71,42 @@ def get_verified_tweets():
 
     except Exception as exp:
         if ERROR_KEY in return_status:
-            error_msg = "Error while fetching verified tweets in views", str(return_status[ERROR_KEY])
+            error_msg = "Error while fetching verified tweets in views: ", str(return_status[ERROR_KEY])
             print(error_msg)
         else:
-            error_msg = "Error while fetching verified tweets in views" + str(exp)
+            error_msg = "Error while fetching verified tweets in views: " + str(exp)
+            print(error_msg)
+            return_status[STATUS_KEY] = STATUS_FAILED
+            return_status[ERROR_KEY] = error_msg
+            return jsonify(return_status)
+
+
+@application.route('/tweets_by_language', methods=['GET', 'POST'])
+def get_tweets_by_language():
+    return_status = {}
+    result = []
+    try:
+        lang = request.args.get('lang')
+
+        if (lang == None or lang == ""): 
+            raise(Exception("Required parameter 'lang' was not specified"))
+
+        return_status = utils_obj.get_verified_tweets()
+
+        if return_status[STATUS_KEY] == STATUS_FAILED:
+            raise Exception(return_status[ERROR_KEY])
+
+        print("Tweets in language ", lang, ":")
+        print(return_status[RESULT_KEY])
+
+        return jsonify(return_status)
+
+    except Exception as exp:
+        if ERROR_KEY in return_status:
+            error_msg = "Error while fetching tweets by language in views: ", str(return_status[ERROR_KEY])
+            print(error_msg)
+        else:
+            error_msg = "Error while fetching tweets by language in views: " + str(exp)
             print(error_msg)
             return_status[STATUS_KEY] = STATUS_FAILED
             return_status[ERROR_KEY] = error_msg
