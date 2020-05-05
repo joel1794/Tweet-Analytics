3ffe1314895e6b514188621b5cc852bd3be0ab74
Micah <mdemong1@gmail.com>
2020-05-05 13:09:19 -0700

    added functions from master in attempt to resolve complex merge conflict

--- ViewsUtils.py
+++ ViewsUtils.py
@@ -47,6 +47,74 @@ class ViewsUtils:
             return utility_status
 
 
+    def deleted_tweets(self):
+        utility_status = {}
+        try:
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query = [{"delete" : {"$exists": True}}]
+            query_type = "select"
+            result = mongo_con.execute_query(query, query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            utility_status[RESULT_KEY] = []
+
+            for record in result[RESULT_KEY]:
+                if "_id" in record:
+                    del record["_id"]
+                utility_status[RESULT_KEY].append(record)
+
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while fetching deleted tweets in viewsutils " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while fetching deleted tweets in viewsutils" + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
+
+
+    def tweets_with_urls(self):
+        utility_status = {}
+        try:
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query = [{"entities.urls": {"$exists": True, "$ne": []}}]
+            query_type = "select"
+            result = mongo_con.execute_query(query, query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            utility_status[RESULT_KEY] = []
+
+            for record in result[RESULT_KEY]:
+                if "_id" in record:
+                    del record["_id"]
+                utility_status[RESULT_KEY].append(record)
+
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while fetching tweets with urls in viewsutils " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while fetching tweets with urls in viewsutils" + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
+
+
     def fetch_verified_tweets(self):
         utility_status = {}
         try:
--- views.py
+++ views.py
@@ -48,6 +48,70 @@ def fetch_tweets_with_hashtags():
             return jsonify(return_status)
 
 
+@application.route('/deleted_tweets', methods=['GET', 'POST'])
+def fetch_deleted_tweets():
+    return_status = {}
+    result = []
+    try:
+        return_status = utils_obj.deleted_tweets()
+
+        if return_status[STATUS_KEY] == STATUS_FAILED:
+            raise Exception(return_status[ERROR_KEY])
+
+        print("Deleted tweets are as follows:")
+
+        print(return_status[RESULT_KEY])
+
+
+        # return_status[RESULT_KEY] = result
+
+        # print(return_status)
+        return jsonify(return_status)
+
+    except Exception as exp:
+        if ERROR_KEY in return_status:
+            error_msg = "Error while fetching deleted tweets in views", str(return_status[ERROR_KEY])
+            print(error_msg)
+        else:
+            error_msg = "Error while fetching deleted tweets in views" + str(exp)
+            print(error_msg)
+            return_status[STATUS_KEY] = STATUS_FAILED
+            return_status[ERROR_KEY] = error_msg
+            return jsonify(return_status)
+
+
+@application.route('/tweets_with_urls', methods=['GET', 'POST'])
+def fetch_tweets_with_urls():
+    return_status = {}
+    result = []
+    try:
+        return_status = utils_obj.tweets_with_urls()
+
+        if return_status[STATUS_KEY] == STATUS_FAILED:
+            raise Exception(return_status[ERROR_KEY])
+
+        print("Tweets with urls are as follows:")
+
+        print(return_status[RESULT_KEY])
+
+
+        # return_status[RESULT_KEY] = result
+
+        # print(return_status)
+        return jsonify(return_status)
+
+    except Exception as exp:
+        if ERROR_KEY in return_status:
+            error_msg = "Error while fetching Tweets with urls in views", str(return_status[ERROR_KEY])
+            print(error_msg)
+        else:
+            error_msg = "Error while fetching Tweets with urls in views" + str(exp)
+            print(error_msg)
+            return_status[STATUS_KEY] = STATUS_FAILED
+            return_status[ERROR_KEY] = error_msg
+            return jsonify(return_status)
+
+
 @application.route('/verified_tweets', methods=['GET', 'POST'])
 def fetch_verified_tweets():
     return_status = {}
