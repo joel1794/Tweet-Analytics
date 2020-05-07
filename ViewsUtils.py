418c191210dee5408ca6c355231ae2e972673f91
rabiamohiuddin <rabia@mohiuddin.com>
2020-05-06 23:15:34 -0700

    Added functions from master to resolve merge conflict

--- ViewsUtils.py
+++ ViewsUtils.py
@@ -282,6 +282,157 @@ class ViewsUtils:
             return utility_status
 
 
+    def fetch_tweets_with_certain_text(self, language, text):
+        utility_status = {}
+        lang_to_langcode = {"Arabic": "ar", "Bulgarian": "bg", "Catalan": "ca", "Czech": "cs", "Welsh": "cy",
+                            "Danish": "da", "German": "de", "Greek": "el",
+                            "English": ["en", "en-au", "en-gb", "en-gb"], "Filipino": "fil",
+                            "Spanish": ["es", "es-mx"], "Basque": "eu", "Farsi": "fa", "Finnish": "fi",
+                            "French": "fr", "Irish": "ga", "Galician": "gl", "Gujarati": "gu", "Hebrew": "he",
+                            "Hindi": "hi", "Croatian": "hr", "Hungarian": "hu", "Indonesian": "id", "Italian": "it",
+                            "Japanese": "ja", "Kannada": "kn", "Korean": "ko", "Latvian": "lv", "Marathi": "mr",
+                            "Malay": ["ms", "msa"], "Norwegian": ["nb", "no"], "Dutch": "nl", "Bengali": "bn",
+                            "Polish": "pl", "Portuguese": ["pt", "pt-pt"], "Romanian": "ro", "Russian": "ru",
+                            "Slovak": "sk", "Serbian": "sr", "Swedish": "sv", "Tamil": "ta", "Thai": "th",
+                            "Turkish": "tr",
+                            "Ukrainian": "uk", "Urdu": "ur", "Vietnamese": "vi", "Xhosa": "xx-lc",
+                            "Chinese": ["zh-cn", "zh-hk", "zh-hans", "zh-hant"]}
+        try:
+            lang_codes = lang_to_langcode[language]
+            if isinstance(lang_codes, str):
+                lang_codes = [lang_codes]
+
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query = [{"$and": [{"user.lang": {"$in": lang_codes}}, {"text": {"$regex": ".*text.*i"}}]}]
+            query_type = "select"
+            result = mongo_con.execute_query(query, query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            utility_status[RESULT_KEY] = []
+
+            for record in result[RESULT_KEY]:
+                if "_id" in record:
+                    del record["_id"]
+                utility_status[RESULT_KEY].append(record)
+
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while fetching tweets with certain text in viewsutils: " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while fetching tweets with certain text in viewsutils: " + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
+
+    def insert_tweet(self, tweet_params):
+        utility_status = {}
+        try:
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query_type = "insert"
+            print(tweet_params)
+            print(type(tweet_params))
+            result = mongo_con.execute_query(json.loads(json.dumps(tweet_params)), query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            utility_status[RESULT_KEY] = result[RESULT_KEY]
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while inserting tweet in viewsutils " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while inserting tweet with hashtags in viewsutils" + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
+
+    def remove_tweet(self, tweet_params):
+        utility_status = {}
+        try:
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query_type = "count"
+            result = mongo_con.execute_query(json.loads(json.dumps(tweet_params)), query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            print(result[RESULT_KEY], "number of tweets will be deleted")
+            query_type = "delete"
+            result = mongo_con.execute_query(tweet_params, query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            utility_status[RESULT_KEY] = result[RESULT_KEY]
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while deleting tweets in viewsutils " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while deleting tweets in viewsutils" + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
+
+    def find_tweets_for_user_ordered(self, user_id):
+        utility_status = {}
+        try:
+
+            mongo_url = "mongodb://localhost:27023/"
+            mongo_con = MongoDBUtility(mongo_url, DATABASE_NAME, COLLECTION_NAME)
+            query = [{"id_str": str(user_id)}]
+            query_type = "select"
+            result = mongo_con.execute_query(query, query_type)
+
+            if result[STATUS_KEY] == STATUS_FAILED:
+                raise Exception(result[ERROR_KEY])
+
+            date_time_dict = {}
+            for record in result[RESULT_KEY]:
+                if "_id" in record:
+                    del record["_id"]
+
+                datetime_obj = datetime.strptime(record["created_at"], "%a %b %d %H:%M:%S %z %Y")
+                date_time_dict[datetime_obj] = record
+
+            utility_status[RESULT_KEY] = []
+            for date_obj in sorted(date_time_dict.keys(), reverse=True):
+                utility_status[RESULT_KEY].append(date_time_dict[date_obj])
+
+            utility_status[STATUS_KEY] = STATUS_SUCCESS
+            return utility_status
+
+        except Exception as exp:
+            if ERROR_KEY in utility_status:
+                error_msg = "Error while fetching tweets by language in viewsutils: " + str(utility_status[ERROR_KEY])
+                print(error_msg)
+            else:
+                error_msg = "Error while fetching tweets by language in viewsutils: " + str(exp)
+                print(error_msg)
+            utility_status[STATUS_KEY] = STATUS_FAILED
+            utility_status[ERROR_KEY] = error_msg
+            return utility_status
+
+
     def fetch_tweets_by_min_favorites(self, minimum):
         utility_status = {}
 
